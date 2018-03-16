61846cd748ac50f0c9d41eb0cfdc6e9ac90210d9
Alexey Simatov <alexsim1993@gmail.com>
2018-03-16 20:38:42 +0400

    Init commit. DOM

---
+++ 10_DOM/index.js
@@ -0,0 +1,16 @@
+"use strict";
+(function getHTML(){
+  let container = document.querySelector('#container');
+  let htmlCode = document.querySelector('#html-code');
+  let addButton =  document.querySelector('#add-button');
+  let htmlCode = document.querySelector('#add-html-code');
+  function getElement(){
+    let 
+  }
+  addButton.addEventListener('click', function(){
+
+  });
+  htmlCode.addEventListener('click', function(){
+
+  });
+})();
