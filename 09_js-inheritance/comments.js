0e8eff6400287cfe914d2b04e5e921dc40642bdb
Aleksei <alexsim1993@gmail.com>
2018-03-17 14:40:26 +0400

    Исправил поле name, text. Исправил имя commentIsHidden. Переместил методы прототипа

--- 09_js-inheritance/comments.js
+++ 09_js-inheritance/comments.js
@@ -8,41 +8,38 @@ Comment.prototype.quote = function (userName) {
 Comment.prototype.render = function () {
   return `${this.author} написал: ${this.text}`;
 };
-function UserComment(name, text, date){
-  Comment.apply(this, arguments);
-  this.name = name;
-  this.text = text;
-  this.date = date;
-  this.rate = 0;
-};
-function AdComment(name, text, link){
-  Comment.apply(this, arguments);
-  this.name = name;
-  this.text = text;
-  this.link = link;
-  this.commentIsHidden = false;
-};
 function inherit(Child, Parent){
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 };
 inherit(AdComment, Comment);
 inherit(UserComment, Comment);
-
+function UserComment(name, text, date){
+  Comment.apply(this, arguments);
+  this.date = date;
+  this.rate = 0;
+};
+UserComment.prototype.render = function () {
+  return `${this.author} написал ${this.date.getDate()}.${this.date.getMonth() + 1}.${this.date.getFullYear()}: ${this.text}; Рейтинг: ${this.rate}`;
+};
 UserComment.prototype.like = function () {
   this.rate++;
 };
 UserComment.prototype.dislike = function () {
   this.rate--;
 };
-UserComment.prototype.render = function () {
-  return `${this.name} написал ${this.date.getDate()}.${this.date.getMonth() + 1}.${this.date.getFullYear()}: ${this.text}; Рейтинг: ${this.rate}`;
+
+function AdComment(name, text, link){
+  Comment.apply(this, arguments);
+  this.link = link;
+  this.isHidden = false;
+};
+
+AdComment.prototype.render = function () {
+  return !this.isHidden?`${this.author} написал: ${this.text}; Перейти >> ${this.link}`:'Реклама скрыта';
 };
 AdComment.prototype.hide = function () {
-  this.commentIsHidden = true;
-};
-AdComment.prototype.render = function () {
-return !this.commentIsHidden?`${this.name} написал: ${this.text}; Перейти >> ${this.link}`:'Реклама скрыта';
+  this.isHidden = true;
 };
 
 exports.Comment = Comment;
