1a9ce586c50acb0d4660abe4b267bef7d949a38c
Alexey Simatov <alexsim1993@gmail.com>
2018-03-20 20:38:11 +0400

    ES6. Ready version

---
+++ 11_ES6/index.js
@@ -0,0 +1,550 @@
+const response = {
+  "users": [
+    {
+      "id": "5ab0cc707ad6d8813bc3ccdf",
+      "index": 0,
+      "guid": "35b36528-6956-47b3-abfa-5625cd9d29ba",
+      "isActive": false,
+      "name": "Phoebe",
+      "surname": "Flynn",
+      "age": 29,
+      "phone": "(868) 585-2783",
+      "registered": "2014-08-22T07:29:59 -04:00",
+      "jobPosition": "L3",
+      "division": "JavaScript",
+      "englishLevel": "B2"
+    },
+    {
+      "id": "5ab0cc70f26194d3cbf34d5f",
+      "index": 1,
+      "guid": "165bd3e3-3f89-4d38-98f9-3c4d78e17b3b",
+      "isActive": false,
+      "name": "Lolita",
+      "surname": "Boyer",
+      "age": 38,
+      "phone": "(821) 469-3631",
+      "registered": "2016-09-25T11:54:50 -04:00",
+      "jobPosition": "L3",
+      "division": "JavaScript",
+      "englishLevel": "C1"
+    },
+    {
+      "id": "5ab0cc70fcfefbc77befb089",
+      "index": 2,
+      "guid": "3c99ede9-9239-43bf-a595-1ed102b8084d",
+      "isActive": true,
+      "name": "Marylou",
+      "surname": "Flowers",
+      "age": 25,
+      "phone": "(955) 539-3583",
+      "registered": "2014-07-18T04:37:36 -04:00",
+      "jobPosition": "L2",
+      "division": ".NET",
+      "englishLevel": "C1"
+    },
+    {
+      "id": "5ab0cc7078c89991b93b247c",
+      "index": 3,
+      "guid": "0781e5d4-c685-4140-af85-97855b7b30e8",
+      "isActive": false,
+      "name": "Sybil",
+      "surname": "Fry",
+      "age": 26,
+      "phone": "(847) 450-2031",
+      "registered": "2012-12-03T07:23:00 -04:00",
+      "jobPosition": "L2",
+      "division": ".NET",
+      "englishLevel": "B2"
+    },
+    {
+      "id": "5ab0cc70fa818e4eb56b376a",
+      "index": 4,
+      "guid": "396c9155-f916-476a-9891-99d8c0d82f01",
+      "isActive": true,
+      "name": "Josie",
+      "surname": "Bailey",
+      "age": 29,
+      "phone": "(851) 561-2855",
+      "registered": "2011-10-31T08:13:40 -04:00",
+      "jobPosition": "L3",
+      "division": "Front-end",
+      "englishLevel": "A1"
+    },
+    {
+      "id": "5ab0cc7044182725702543f9",
+      "index": 5,
+      "guid": "066836ca-7aaf-4542-aafc-c622f6a645de",
+      "isActive": false,
+      "name": "Rhodes",
+      "surname": "Terry",
+      "age": 39,
+      "phone": "(990) 456-2154",
+      "registered": "2016-03-09T03:19:59 -04:00",
+      "jobPosition": "L1",
+      "division": "Java",
+      "englishLevel": "A1"
+    },
+    {
+      "id": "5ab0cc703ac357c752aeb67e",
+      "index": 6,
+      "guid": "fb752bdf-54bf-41c0-a59d-510de0dc2728",
+      "isActive": true,
+      "name": "Marva",
+      "surname": "Morrison",
+      "age": 38,
+      "phone": "(888) 488-2256",
+      "registered": "2016-01-28T08:18:15 -04:00",
+      "jobPosition": "L3",
+      "division": "JavaScript",
+      "englishLevel": "A1"
+    },
+    {
+      "id": "5ab0cc702eb0e18138d2f18a",
+      "index": 7,
+      "guid": "7a0426a1-08c6-4807-9e21-141218ce565d",
+      "isActive": true,
+      "name": "Rush",
+      "surname": "Stanley",
+      "age": 29,
+      "phone": "(884) 468-3410",
+      "registered": "2013-04-14T01:41:15 -04:00",
+      "jobPosition": "L3",
+      "division": "Front-end",
+      "englishLevel": "C1"
+    },
+    {
+      "id": "5ab0cc70224bd09991a85fb4",
+      "index": 8,
+      "guid": "7decaaa4-0243-4565-bddc-a7f0b9936b89",
+      "isActive": true,
+      "name": "Michelle",
+      "surname": "Carney",
+      "age": 34,
+      "phone": "(962) 473-2485",
+      "registered": "2013-11-12T06:01:50 -04:00",
+      "jobPosition": "L2",
+      "division": "Front-end",
+      "englishLevel": "B2"
+    },
+    {
+      "id": "5ab0cc709515a83ec638aef6",
+      "index": 9,
+      "guid": "de8cd1d3-0e87-4f86-a949-e2ee8e3894b3",
+      "isActive": true,
+      "name": "Pierce",
+      "surname": "Cannon",
+      "age": 27,
+      "phone": "(848) 517-2736",
+      "registered": "2013-06-27T06:42:04 -04:00",
+      "jobPosition": "L3",
+      "division": "Front-end",
+      "englishLevel": "A2"
+    },
+    {
+      "id": "5ab0cc707b09a6faf1f076fb",
+      "index": 10,
+      "guid": "05b5f599-10ff-4889-9c7a-a23aac47194a",
+      "isActive": false,
+      "name": "Suzanne",
+      "surname": "Herman",
+      "age": 32,
+      "phone": "(944) 499-2550",
+      "registered": "2013-05-16T03:17:56 -04:00",
+      "jobPosition": "L2",
+      "division": "Java",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc70970aa1843a5df3d6",
+      "index": 11,
+      "guid": "0183b009-f8db-45a9-a352-40336d952526",
+      "isActive": true,
+      "name": "Juliana",
+      "surname": "Berg",
+      "age": 32,
+      "phone": "(883) 541-3132",
+      "registered": "2011-03-10T11:05:49 -04:00",
+      "jobPosition": "L2",
+      "division": "SQL",
+      "englishLevel": "A1"
+    },
+    {
+      "id": "5ab0cc7073ec082be42ac22b",
+      "index": 12,
+      "guid": "24ba20f6-76b4-4aed-a2f2-47074ceb3899",
+      "isActive": true,
+      "name": "Rosario",
+      "surname": "Bates",
+      "age": 40,
+      "phone": "(961) 536-3720",
+      "registered": "2011-11-01T08:02:33 -04:00",
+      "jobPosition": "L1",
+      "division": "Java",
+      "englishLevel": "A1"
+    },
+    {
+      "id": "5ab0cc701f7b5bf89148a5bb",
+      "index": 13,
+      "guid": "a4fe92b2-0078-4c40-95b3-af714c7075dd",
+      "isActive": false,
+      "name": "Dalton",
+      "surname": "Humphrey",
+      "age": 33,
+      "phone": "(885) 586-2556",
+      "registered": "2011-10-16T09:17:41 -04:00",
+      "jobPosition": "L1",
+      "division": "SQL",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc70586f6e7bc6f4503a",
+      "index": 14,
+      "guid": "75c83145-1937-4332-95b5-26c2bf5ead76",
+      "isActive": true,
+      "name": "Mayra",
+      "surname": "Newton",
+      "age": 25,
+      "phone": "(826) 536-3364",
+      "registered": "2012-10-03T04:55:07 -04:00",
+      "jobPosition": "L3",
+      "division": "Front-end",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc70391b2321769cea97",
+      "index": 15,
+      "guid": "6865d360-598b-464f-9dc4-3ee1e9cb293d",
+      "isActive": true,
+      "name": "Payne",
+      "surname": "Melton",
+      "age": 39,
+      "phone": "(828) 450-3759",
+      "registered": "2014-07-14T09:49:41 -04:00",
+      "jobPosition": "L3",
+      "division": "JavaScript",
+      "englishLevel": "A2"
+    },
+    {
+      "id": "5ab0cc702c0e1980507d9083",
+      "index": 16,
+      "guid": "48b572e4-0991-4963-839a-93a1b14670b2",
+      "isActive": true,
+      "name": "Hardin",
+      "surname": "Morrow",
+      "age": 32,
+      "phone": "(849) 482-2634",
+      "registered": "2012-01-07T11:21:19 -04:00",
+      "jobPosition": "L2",
+      "division": "SQL",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc70a6d0cbb3d7332145",
+      "index": 17,
+      "guid": "61fe40d5-8097-42c9-b6f7-51be3d8680fc",
+      "isActive": true,
+      "name": "Gardner",
+      "surname": "Garrett",
+      "age": 31,
+      "phone": "(954) 450-3717",
+      "registered": "2014-06-06T08:47:28 -04:00",
+      "jobPosition": "L1",
+      "division": ".NET",
+      "englishLevel": "C1"
+    },
+    {
+      "id": "5ab0cc70b66b8a039f4bc0a6",
+      "index": 18,
+      "guid": "ee90007b-7197-4931-ab39-e2eee2e55ee7",
+      "isActive": false,
+      "name": "Glover",
+      "surname": "Rodriquez",
+      "age": 26,
+      "phone": "(920) 464-2545",
+      "registered": "2017-04-28T05:23:47 -04:00",
+      "jobPosition": "L3",
+      "division": "Front-end",
+      "englishLevel": "A2"
+    },
+    {
+      "id": "5ab0cc70b4a3a021b6a2b2c9",
+      "index": 19,
+      "guid": "503efe92-6e01-4a31-8aed-7993a6cf287c",
+      "isActive": true,
+      "name": "Green",
+      "surname": "Mcdonald",
+      "age": 25,
+      "phone": "(927) 476-3231",
+      "registered": "2018-01-28T02:30:04 -04:00",
+      "jobPosition": "L2",
+      "division": "SQL",
+      "englishLevel": "B2"
+    },
+    {
+      "id": "5ab0cc70a506305784ba79d3",
+      "index": 20,
+      "guid": "349ab591-fc70-42dd-86c2-c4b5c441650b",
+      "isActive": false,
+      "name": "Herman",
+      "surname": "Massey",
+      "age": 25,
+      "phone": "(821) 456-2970",
+      "registered": "2013-03-19T03:40:36 -04:00",
+      "jobPosition": "L2",
+      "division": ".NET",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc7092e0d569f1cb4655",
+      "index": 21,
+      "guid": "67fa30ed-a813-41a8-9761-f74554e6e7c1",
+      "isActive": true,
+      "name": "Hartman",
+      "surname": "Henderson",
+      "age": 28,
+      "phone": "(917) 501-2723",
+      "registered": "2012-10-21T10:45:50 -04:00",
+      "jobPosition": "L2",
+      "division": "Front-end",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc702b95c66fad4b486e",
+      "index": 22,
+      "guid": "6833b24a-348f-4dff-9eac-16db63d2deae",
+      "isActive": false,
+      "name": "Whitney",
+      "surname": "Kramer",
+      "age": 39,
+      "phone": "(950) 403-2645",
+      "registered": "2016-09-19T09:33:43 -04:00",
+      "jobPosition": "L2",
+      "division": "JavaScript",
+      "englishLevel": "C1"
+    },
+    {
+      "id": "5ab0cc70388864bc58937a89",
+      "index": 23,
+      "guid": "fa0584bc-ac40-4191-8aa9-574c3eb3544c",
+      "isActive": true,
+      "name": "Katina",
+      "surname": "Wolfe",
+      "age": 28,
+      "phone": "(823) 570-2601",
+      "registered": "2018-01-08T07:58:56 -04:00",
+      "jobPosition": "L3",
+      "division": "Java",
+      "englishLevel": "B2"
+    },
+    {
+      "id": "5ab0cc703a864ac8296d8cf5",
+      "index": 24,
+      "guid": "4e64a103-dfb4-4d08-a68c-34af3fd5319b",
+      "isActive": true,
+      "name": "Adela",
+      "surname": "Hoffman",
+      "age": 22,
+      "phone": "(972) 409-3084",
+      "registered": "2018-03-08T10:55:36 -04:00",
+      "jobPosition": "L2",
+      "division": "Java",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc70cd9b4a54442a9b99",
+      "index": 25,
+      "guid": "b9db8e6f-d94d-4b8a-98fd-91922a5fbb03",
+      "isActive": true,
+      "name": "Compton",
+      "surname": "Vincent",
+      "age": 27,
+      "phone": "(887) 426-3431",
+      "registered": "2011-11-06T06:04:25 -04:00",
+      "jobPosition": "L1",
+      "division": "Java",
+      "englishLevel": "B1"
+    },
+    {
+      "id": "5ab0cc701a2df6e2eb03207c",
+      "index": 26,
+      "guid": "14eae1e0-d970-4afd-ba76-915027365122",
+      "isActive": false,
+      "name": "Cortez",
+      "surname": "David",
+      "age": 37,
+      "phone": "(866) 404-2161",
+      "registered": "2011-05-16T01:33:52 -04:00",
+      "jobPosition": "L1",
+      "division": "JavaScript",
+      "englishLevel": "A1"
+    },
+    {
+      "id": "5ab0cc709e405086724d4997",
+      "index": 27,
+      "guid": "4bb6d112-b913-4726-a8b4-ac23520e1a4d",
+      "isActive": true,
+      "name": "Griffith",
+      "surname": "Fuentes",
+      "age": 38,
+      "phone": "(888) 535-3382",
+      "registered": "2014-09-30T11:02:54 -04:00",
+      "jobPosition": "L1",
+      "division": "JavaScript",
+      "englishLevel": "B2"
+    },
+    {
+      "id": "5ab0cc70edb9f351deeac1ec",
+      "index": 28,
+      "guid": "dac8d3c7-d11d-4161-b168-947e0bce32cd",
+      "isActive": true,
+      "name": "Shelia",
+      "surname": "Walker",
+      "age": 40,
+      "phone": "(964) 548-2965",
+      "registered": "2011-08-28T06:23:32 -04:00",
+      "jobPosition": "L3",
+      "division": "Java",
+      "englishLevel": "C1"
+    },
+    {
+      "id": "5ab0cc70463242a4b9365266",
+      "index": 29,
+      "guid": "1b435d74-d836-4294-a39a-2b456bd648a4",
+      "isActive": false,
+      "name": "Daniel",
+      "surname": "Parsons",
+      "age": 23,
+      "phone": "(881) 423-2511",
+      "registered": "2013-02-03T10:26:05 -04:00",
+      "jobPosition": "L3",
+      "division": "Front-end",
+      "englishLevel": "B2"
+    }
+  ],
+  "projects": [
+    {
+      "id": "5ab0e0fb7da8a65774a26ef5",
+      "index": 0,
+      "name": "Comstruct",
+      "DM": "Terra James",
+      "technology": ".NET",
+      "isActive": true
+    },
+    {
+      "id": "5ab0e0fb86d290a37725473a",
+      "index": 1,
+      "name": "Entropix",
+      "DM": "Morin Pena",
+      "technology": "Front-end",
+      "isActive": false
+    },
+    {
+      "id": "5ab0e0fbd7727c49c4646246",
+      "index": 2,
+      "name": "Acrodance",
+      "DM": "England Kirby",
+      "technology": "Java",
+      "isActive": false
+    },
+    {
+      "id": "5ab0e0fbbe04fa1a3f58c0e5",
+      "index": 3,
+      "name": "Zilodyne",
+      "DM": "Jacobson Mcconnell",
+      "technology": "Front-end",
+      "isActive": true
+    },
+    {
+      "id": "5ab0e0fb1f2cec3b2dcaf698",
+      "index": 4,
+      "name": "Centrexin",
+      "DM": "Alma Harding",
+      "technology": "SQL",
+      "isActive": true
+    },
+    {
+      "id": "5ab0e0fb87ba59a4809fb356",
+      "index": 5,
+      "name": "Illumity",
+      "DM": "Lela Koch",
+      "technology": "Java",
+      "isActive": true
+    },
+    {
+      "id": "5ab0e0fb196fdf5dac84a254",
+      "index": 6,
+      "name": "Nixelt",
+      "DM": "Ladonna Mccarthy",
+      "technology": "Java",
+      "isActive": false
+    },
+    {
+      "id": "5ab0e0fb0d08f1f5067c85c7",
+      "index": 7,
+      "name": "Daycore",
+      "DM": "Allyson Griffith",
+      "technology": "Java",
+      "isActive": true
+    },
+    {
+      "id": "5ab0e0fb2bae602283cc7506",
+      "index": 8,
+      "name": "Momentia",
+      "DM": "Santiago Maynard",
+      "technology": ".NET",
+      "isActive": true
+    },
+    {
+      "id": "5ab0e0fb80a40d3344599498",
+      "index": 9,
+      "name": "Viagreat",
+      "DM": "Irma Pugh",
+      "technology": "SQL",
+      "isActive": true
+    }
+  ]
+};
+
+let {users: users, projects: projects} = response;
+
+(function init() {
+  users.forEach((user) => user.email = `${user.name}_${user.surname}@company.com`);
+  projects.forEach((project) => project.workers = users.filter((user)=>user.division === project.technology));
+
+	/*
+  	1) С помощью деструктурирования инициализовать переменные users и projects соответствующими массивами из объекта response
+    2) Переборы массивов осуществлять либо одним из ES6-способов, либо forEach со стрелочной функцией
+    3) Добавить в каждый объект user свойство email, которое должно соответствовать следующему паттерну: <name>_<surname>@company.com; пример - Doe@company.com.
+    	3.1) Обязательно использование шаблонные строки, использование конкатенации запрещается
+    4) Добавить в каждый объект project свойство workers - массив  юзеров из переменной users, у которых поле division совпадает с полем technology в проекте
+    	4.1) Советую использовать фильтр: Array.prototype.filter()
+  */
+
+})();
+
+/*
+	Все геттеры должны быть стрелочными функциями
+  Более предпочитаемы однострочные функции
+*/
+const getActiveUsers = () => users.filter(user => user.isActive === true);
+const getActiveProjects = () => projects.filter(project => project.isActive === true);
+const getProjectsByTechnology = (technology) => projects.filter(project => project.technology === technology);
+const getExperiencedWorkers = (project, jobPosition) => projects.find(element=>element.id === project).workers.filter(worker => worker.jobPosition === jobPosition);
+
+
+function printUserData(userIndex) {
+  let user = users.find((element)=>element.index === userIndex);
+  console.log(`
+    Full Name: ${user.name} ${user.surname}
+    Position: ${user.jobPosition}
+    Division: ${user.division}
+    English level|: ${user.englishLevel}
+  `);
+	/*
+  	Вывести информацию о юзере в консоль в несколько строчек по следующему шаблону:
+    Full name:
+    Position:
+    Division:
+    English level:
+  */
+}
