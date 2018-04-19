3fd6b82ba1942c9d3d6fbed8d0975803336821b9
Aleksei <alexsim1993@gmail.com>
2018-04-20 01:11:53 +0400

    Add error/catch for fetch

--- 20_HOC/my-app/src/withData.jsx
+++ 20_HOC/my-app/src/withData.jsx
@@ -7,12 +7,18 @@ export const withData = (mapUrlsToProps) => (WrappedComponent) =>(
         static displayName = 'AppWithData';
         fetchData = () => {
             map(mapUrlsToProps, (propDef, key) => {
-                fetch(propDef.url)
-                    .then(response =>  response.json())
+                fetch('propDef.url')
+                    .then(response =>  {
+                        if(response.ok){
+                            return response.json();
+                        }
+                        throw new Error(response.statusText);
+                    })
                     .then((data) => this.setState({ 
                         [key]: get(data, propDef.path , {currently: {data: 'no data'}}),
                         time: new Date(get(data, 'currently.time', 0) * 1000)
                     }))
+                    .catch(err => console.log('Error in AJAX request', err))
             });
         }
         updateData = () => {
