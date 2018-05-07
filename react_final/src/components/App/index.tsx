fc26861d7ecc464f8c8d1ae06e6135d1a3dcf398
Aleksei <alexsim1993@gmail.com>
2018-05-08 00:58:44 +0400

    remove default app

---
+++ react_final/src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const App = () => (
+    <div>
+        Example
+    </div>
+);
+
+export default App;
--- react_final/src/index.tsx
+++ react_final/src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App/index';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </ BrowserRouter>
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
